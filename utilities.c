1cca88e2b51bb7dff4942f11592c1bbd5b5ac00a
H0xC0000005 <zhaopeizhusg@gmail.com>
2022-10-17 20:29:12 +0800

    add utilities slots

---
+++ utilities.c
@@ -0,0 +1,13 @@
+#include "utilities.h"
+
+
+void output_bstr_as_hex(char* binstr, int does_print){
+    char digest[32];
+    if (does_print){
+        for (unsigned int i = 0; i < 16; ++i) {
+            printf("%02x", (int)(binstr[i]) & 0xFF);
+        }
+        return 0;
+    }
+    
+}
---
+++ utilities.h
@@ -0,0 +1,5 @@
+
+
+char* binstr_to_hexdigest(char* binstr);
+
+
